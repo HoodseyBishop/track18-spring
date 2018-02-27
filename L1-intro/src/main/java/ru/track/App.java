e0d6748400368769673dac344f1174d14b8473b9
Unisov Ilya <ilya.unisov@gmail.com>
2018-02-28 01:25:17 +0300

    intro

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -9,20 +9,25 @@ package ru.track;
  *
  *
  */
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
 public class App {
 
     public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_GITHUB = "github";
-    public static final String FIELD_EMAIL = "email";
+    public static final String FIELD_NAME = "Ilya";
+    public static final String FIELD_GITHUB = "HoodseyBishop";
+    public static final String FIELD_EMAIL = "ilya,unisov@gmail.com";
 
     public static void main(String[] args) throws Exception {
-       // 1) Use Unirest.post()
-        // 2) Get response .asJson()
-        // 3) Get json body and JsonObject
-        // 4) Get field "success" from JsonObject
-
-
+        HttpResponse<JsonNode> r =  Unirest.post(URL)
+                .field("name", FIELD_NAME)
+                .field("github", FIELD_GITHUB)
+                .field("email", FIELD_EMAIL)
+                .asJson();
+        System.out.println(r.getBody().getObject().get("success"));
 
 
         boolean success = false;
